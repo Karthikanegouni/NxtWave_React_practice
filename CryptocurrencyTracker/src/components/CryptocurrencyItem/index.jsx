2e422d8d8903240f27f2ce188b37829bbb7b9096
Anegouni Karthik Goud <karthikanegouni@gmail.com>
2025-07-17 18:56:26 +0530

    implements CryptoTracker

--- CryptocurrencyTracker/src/components/CryptocurrencyItem/index.jsx
+++ CryptocurrencyTracker/src/components/CryptocurrencyItem/index.jsx
@@ -1,5 +1,16 @@
-import React from "react"
+import "./index.css"
 
-export default function CryptocurrencyItem() {
-  return <div>CryptocurrencyItem</div>
+export default function CryptocurrencyItem({ currencyData }) {
+  const { currencyName, currencyLogo, usdValue, euroValue } = currencyData
+
+  return (
+    <li className="cryptocurrency-item-cnt">
+      <div className="currency-type">
+        <img src={currencyLogo} alt={currencyName} className="currency-logo" />
+        <p className="currency-name">{currencyName}</p>
+      </div>
+      <p className="usd-value">{usdValue}</p>
+      <p className="euro-value">{euroValue}</p>
+    </li>
+  )
 }
