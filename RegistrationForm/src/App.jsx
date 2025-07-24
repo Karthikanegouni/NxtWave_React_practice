7eae15037666c7c1c74594f6e8e344825a13f2b4
Anegouni Karthik Goud <karthikanegouni@gmail.com>
2025-07-24 16:09:48 +0530

    implemented registration form

--- RegistrationForm/src/App.jsx
+++ RegistrationForm/src/App.jsx
@@ -1,5 +1,10 @@
+import RegistrationForm from "./components/RegistrationForm"
 import "./App.css"
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <>
+      <RegistrationForm />
+    </>
+  )
 }
