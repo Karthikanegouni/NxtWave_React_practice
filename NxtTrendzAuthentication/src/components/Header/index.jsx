eaf8c2aa508216bd2f21cc408ee00c88f75e971f
Anegouni Karthik Goud <karthikanegouni@gmail.com>
2025-07-24 14:23:17 +0530

    NXTTrendsAuth implemented

--- NxtTrendzAuthentication/src/components/Header/index.jsx
+++ NxtTrendzAuthentication/src/components/Header/index.jsx
@@ -1,25 +1,42 @@
 import { Link } from "react-router-dom"
 import "./index.css"
 
-export default function Header() {
-  return (
-    <nav className="header-cnt">
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
       <img
+        className="website-logo"
         src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
         alt="website logo"
       />
-      <div className="nav-items">
-        <Link to="/" className="link">
-          <p>Home</p>
-        </Link>
-        <Link to="/products" className="link">
-          <p>Product</p>
-        </Link>
-        <Link to="/cart" className="link">
-          <p>Cart</p>
-        </Link>
-        <button type="button">Logout</button>
-      </div>
-    </nav>
-  )
-}
+      <ul className="nav-menu">
+        <li>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/products" className="nav-link">
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart" className="nav-link">
+            Cart
+          </Link>
+        </li>
+      </ul>
+      <button type="button" className="logout-desktop-btn">
+        Logout
+      </button>
+      <button type="button" className="logout-mobile-btn">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+          alt="logout icon"
+          className="logout-icon"
+        />
+      </button>
+    </div>
+  </nav>
+)
+export default Header
--- NxtTrendzAuthentication/src/components/LoginForm/index.jsx
+++ NxtTrendzAuthentication/src/components/LoginForm/index.jsx
@@ -1,31 +1,132 @@
-import React, { Component } from "react"
+import { Component } from "react"
 import "./index.css"
 
-export default class LoginForm extends Component {
-  render() {
+class LoginForm extends Component {
+  state = {
+    username: "",
+    password: "",
+    showError: false,
+    errorMsg: "",
+  }
+
+  onLoginSuccess = (data) => {
+    const { history } = this.props
+    if (data.jwt_token) {
+      history.replace("/")
+    }
+    this.setState({ username: "", password: "" })
+  }
+
+  onLoginFailure = async (response) => {
+    const data = await response.json()
+    this.setState({
+      showError: true,
+      errorMsg: data.error_msg || "Username or password is invalid",
+    })
+  }
+
+  onSubmitForm = async (event) => {
+    event.preventDefault()
+    const { username, password } = this.state
+    const userDetails = {
+      username,
+      password,
+    }
+
+    const url = "https://apis.ccbp.in/login"
+    const options = {
+      method: "POST",
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    if (response.ok === false) {
+      this.onLoginFailure(response)
+      return
+    }
+    const data = await response.json()
+    console.log(data)
+    this.onLoginSuccess(data)
+  }
+
+  onChangeUsername = (event) => {
+    this.setState({ username: event.target.value })
+  }
+
+  onChangePassword = (event) => {
+    this.setState({ password: event.target.value })
+  }
+
+  renderPasswordField = () => {
+    const { password } = this.state
     return (
-      <div className="login-cnt">
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-filed"
+          placeholder="Password"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const { username } = this.state
+
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-filed"
+          placeholder="Username"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  render() {
+    const { showError, errorMsg } = this.state
+
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
         <img
           src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
           alt="website login"
         />
-        <form className="login-form">
+        <form className="form-container" onSubmit={this.onSubmitForm}>
           <img
             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
             alt="website logo"
           />
-          <div className="input-container">
-            <label htmlFor="username">USERNAME</label>
-            <input type="text" id="username" placeholder="Username"/>
-          </div>
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
 
-          <div className="input-container">
-            <label htmlFor="password">PASSWORD</label>
-            <input type="password" id="password" placeholder="Password" />
-          </div>
-          <button type="submit" className="login-btn">Login</button>
+          {showError && <p className="error-message">*{errorMsg}</p>}
         </form>
       </div>
     )
   }
 }
+
+export default LoginForm
