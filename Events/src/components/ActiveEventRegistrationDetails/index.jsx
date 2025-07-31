ba6b9f68940875e8ebe8144a50659674f5737c59
Anegouni Karthik Goud <karthikanegouni@gmail.com>
2025-07-31 12:21:42 +0530

    final Events App

--- Events/src/components/ActiveEventRegistrationDetails/index.jsx
+++ Events/src/components/ActiveEventRegistrationDetails/index.jsx
@@ -7,62 +7,58 @@ const ActiveEventRegistrationDetails = ({ registrationStatus }) => {
     registrationClosed: "REGISTRATIONS_CLOSED",
   }
 
-  const renderNoActiveEventView = () => {
-    return (
-      <h1 className="noactive-event-text">
-        Click on an event, to view its registration details
+  const renderNoActiveEventView = () => (
+    <p className="noactive-event-text">
+      Click on an event, to view its registration details
+    </p>
+  )
+
+  const renderRegisteredView = () => (
+    <div className="view-cnt">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
+        alt="registered"
+        className="registered-icon"
+      />
+      <h1 className="registered-text">
+        You have already registered for the event
       </h1>
-    )
-  }
+    </div>
+  )
 
-  const renderRegisteredView = () => {
-    return (
-      <div className="view-cnt">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
-          alt="registered"
-          className="registered-icon"
-        />
-        <h1 className="registered-text">
-          You have already registered for the event
-        </h1>
-      </div>
-    )
-  }
+  const renderNotRegisteredView = () => (
+    <div className="view-cnt">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-register-img.png"
+        alt="yet to register"
+        className="not-registered-img"
+      />
+      <p className="info-text">
+        A live performance brings so much to your relationship with dance.
+        Seeing dance live can often make you fall totally in love with this
+        beautiful art form.
+      </p>
+      <button type="button" className="register-btn">
+        Register Here
+      </button>
+    </div>
+  )
 
-  const renderNotRegisteredView = () => {
-    return (
-      <div className="view-cnt">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/events-register-img.png"
-          alt="yet to register"
-          className="not-registered-img"
-        />
-        <p className="info-text">
-          A live performance brings so much to your relationship with dance.
-          Seeing dance live can often make you fall totally in love with this
-          beautiful art form.
-        </p>
-        <button type="button" className="register-btn">
-          Register Here
-        </button>
-      </div>
-    )
-  }
-
-  const renderRegisterationClosedView = () => {
-    return (
-      <div className="view-cnt">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
-          alt="registrations closed"
-          className="registration-closed-img"
-        />
-        <p className="registration-closed-text">Registrations Are Closed Now!</p>
-        <p className="info-text">Stay tuned. We will reopen the registrations soon!</p>
-      </div>
-    )
-  }
+  const renderRegisterationClosedView = () => (
+    <div className="view-cnt">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
+        alt="registrations closed"
+        className="registration-closed-img"
+      />
+      <h1 className="registration-closed-text">
+        Registrations Are Closed Now!
+      </h1>
+      <p className="info-text">
+        Stay tuned. We will reopen the registrations soon!
+      </p>
+    </div>
+  )
 
   const renderEventDetails = () => {
     switch (registrationStatus) {
